e5ec04f1d6be78f83f2685228789c09ab43ea271
Romain Vaur <rvaur@hotmail.com>
2025-03-17 12:07:27 +0100

    evol(MissionSection): Add MissionSection with animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@
 import HeroSection from '@/components/HeroSection';
 import ProductSection from '@/components/ProductSection';
 import Portfolio from '@/components/Portfolio';
+import MissionSection from '@/components/MissionSection';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <MissionSection />
       <Portfolio />
       <ProductSection />
     </main>
