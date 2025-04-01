2d042825ed7251b11ceb4e0a18f9f748354e8110
Romain Vaur <rvaur@hotmail.com>
2025-04-01 10:55:56 +0200

    fix(navigation): update quote button for mobiles

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -13,9 +13,8 @@ const Navigation: React.FC<NavigationProps> = ({ footerRef }) => {
 
   return (
     <nav className="absolute top-0 right-0 w-full z-50 p-4">
-      {/* Version Desktop */}
-      <div className="hidden md:flex justify-end items-center space-x-8 pr-8">
-        <button className="my-1 px-6 py-3 bg-primary text-white rounded-full text-base hover:bg-gray-800 transition-colors" onClick={handleClick}>
+      <div className="flex justify-end items-center space-x-8 md:pr-8">
+        <button className="md:my-1 px-4 py-2 md:px-6 md:py-3 bg-primary text-white rounded-full text-base hover:bg-gray-800 transition-colors" onClick={handleClick}>
           Demander un devis
         </button>
       </div>
