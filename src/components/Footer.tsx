b1c93f96dc20179b7d095cae057f7a0b74af606d
Romain Vaur <rvaur@hotmail.com>
2025-04-01 12:09:07 +0200

    evol(footer): update information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,13 +6,18 @@ import React, { forwardRef } from 'react';
 
 const Footer = forwardRef<HTMLDivElement, React.HTMLProps<HTMLDivElement>>((props, ref) => {
   return (
-    <footer ref={ref} className="bg-black text-white py-16 mt-12">
+    <footer ref={ref} className="bg-primary text-white py-20 mt-12">
       <div className="container mx-auto px-4">
         <div className='flex flex-col lg:flex-row lg:justify-between gap-12'>
           <div className="flex flex-col md:grid md:grid-cols-3 gap-8">
             <div>
-              <h3 className="text-lg font-bold mb-4">UNE COLLABORATION ?</h3>
-              <p className="mb-2">Contactez-nous sur</p>
+              <h3 className="text-lg font-bold mb-4">UN PROJET VIDEO ?</h3>
+              <a
+                href="tel:+33384570226"
+                className="block mt-2 hover:text-gray-300"
+              >
+                03 84 57 02 26
+              </a>
               <a
                 href="mailto:hello@digitaly.pro"
                 className="text-white hover:text-gray-300 font-bold"
@@ -36,25 +41,19 @@ const Footer = forwardRef<HTMLDivElement, React.HTMLProps<HTMLDivElement>>((prop
               <h3 className="text-lg font-bold mb-4">Sevenans</h3>
               <p>6 Rue de Belfort</p>
               <p>90400 Sevenans</p>
-              <a
-                href="tel:+33614788852"
-                className="block mt-2 hover:text-gray-300"
-              >
-                +33 (0)6 14 78 88 52
-              </a>
             </div>
           </div>
 
           <div className="flex flex-col gap-6">
             <div className="flex gap-2 lg:gap-6">
-              <Link href="https://www.instagram.com/oz_digitaly/" className="hover:text-gray-300" target="_blank" rel="noopener noreferrer">
-                <FaInstagram size={20} />
+              <Link href="https://www.youtube.com/@Oz-Digitaly" className="hover:text-gray-300" target="_blank" rel="noopener noreferrer">
+                <FaYoutube size={20} />
               </Link>
               <Link href="https://www.linkedin.com/in/oz-%F0%9F%A6%81-akca-9a0abb154/" className="hover:text-gray-300" target="_blank" rel="noopener noreferrer">
                 <FaLinkedinIn size={20} />
               </Link>
-              <Link href="https://www.youtube.com/@Oz-Digitaly" className="hover:text-gray-300" target="_blank" rel="noopener noreferrer">
-                <FaYoutube size={20} />
+              <Link href="https://www.instagram.com/oz_digitaly/" className="hover:text-gray-300" target="_blank" rel="noopener noreferrer">
+                <FaInstagram size={20} />
               </Link>
             </div>
 
