817e4b29be0b37c9f82148e4111b2fcb922f9cab
Romain Vaur <rvaur@hotmail.com>
2025-04-09 19:13:45 +0200

    evol(design): update bg video, add title and fav, add secondary color.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -14,8 +14,8 @@ const Navigation: React.FC<NavigationProps> = ({ footerRef }) => {
   return (
     <nav className="absolute top-0 right-0 w-full z-50 p-4">
       <div className="flex justify-end items-center space-x-8 md:pr-8">
-        <button className="md:my-1 px-4 py-2 md:px-6 md:py-3 bg-primary text-white rounded-full text-base hover:bg-gray-800 transition-colors" onClick={handleClick}>
-          Demander un devis
+        <button className="md:my-1 px-4 py-2 md:px-6 md:py-3 bg-primary text-white rounded-full text-base hover:bg-secondary hover:text-primary transition-colors duration-300 uppercase" onClick={handleClick}>
+          J&apos;ai un projet
         </button>
       </div>
     </nav>
--- src/components/YoutubeLink.tsx
+++ src/components/YoutubeLink.tsx
@@ -7,7 +7,7 @@ const YoutubeLink = () => {
 
   return (
     <div className="flex justify-center items-center">
-      <button className="my-6 px-6 py-3 bg-primary text-white rounded-full text-base hover:bg-gray-800 transition-colors uppercase" onClick={handleClick}>
+      <button className="my-6 px-6 py-3 bg-primary text-white rounded-full text-base hover:bg-secondary hover:text-primary transition-colors duration-300 uppercase" onClick={handleClick}>
         nos réalisations
       </button>
     </div>
