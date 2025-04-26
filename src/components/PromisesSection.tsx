68b65d311482d42094a795f548d59e46f0646127
Romain Vaur <rvaur@hotmail.com>
2025-04-26 15:21:48 +0200

    Add delay to Promises

--- src/components/PromisesSection.tsx
+++ src/components/PromisesSection.tsx
@@ -38,14 +38,15 @@ const PromisesSection = () => {
       <div
         className={`
           flex flex-col gap-12 text-5xl md:text-5xl font-light text-left xl:text-right text-[#0A0B2E]/80 align-middle
-          transition-all duration-1000 ease-out delay-300
-          ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-12'}
         `}
       >
         <div className=' flex flex-col gap-16'>
-          <p>L&apos;humain est marqué par les <span className="text-secondary font-medium">histoires,</span></p>
-          <p>Car les histoires créent de <span className="text-secondary font-medium">l&apos;émotion,</span></p>
-          <p>Et l&apos;émotion crée de la <span className="text-secondary font-medium">confiance.</span></p>
+          <p className={`transition-all duration-1000 ease-out delay-300
+          ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-12'}`}>L&apos;humain est marqué par les <span className="text-secondary font-medium ">histoires,</span></p>
+          <p className={`transition-all duration-1000 ease-out delay-[700ms]
+          ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-12'}`}>Car les histoires créent de <span className="text-secondary font-medium">l&apos;émotion,</span></p>
+          <p className={`transition-all duration-1000 ease-out delay-[1300ms]
+          ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-12'}`}>Et l&apos;émotion crée de la <span className="text-secondary font-medium">confiance.</span></p>
         </div>
       </div>
 
